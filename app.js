cf552ae2ee7215a24474b37912560f3bad5bdcca
Himanshu Raj <HRaj@ABS-CBN.COM>
2023-08-03 16:49:37 +0530

    adpp codes

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const TorrentSearchApi = require('torrent-search-api');
+
+const app = express();
+app.use(express.json());
+app.use(cors("dev"));
+
+// TorrentSearchApi.enableProvider('Torrent9');
+TorrentSearchApi.enableProvider('1337x');
+
+app.get("/", async (req, res)=>{
+    const torrents = await TorrentSearchApi.search('3 idiots', 'Movies', 20);
+    console.log(torrents);
+    res.send(torrents);
+})
+
+
+
+
+app.listen(3000, (err)=>{
+    if(!err){
+        console.log("App listening on port 3000");
+    }else{
+        console.log("Error in listening app");
+    }
+})
